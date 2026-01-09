2da6c3139d2a909c9acb2b688dc9eebbdf7be22e
IndorewalaA <abdul.indorewala@ufl.edu>
2026-01-09 16:12:38 -0500

    corrected import statement.

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,7 +1,7 @@
 import os
 import boto3
 import json
-from analyzer_logic import validate_json, predict_packet
+from src.analyzer_logic import validate_json, predict_packet
 from dotenv import load_dotenv
 
 load_dotenv()
